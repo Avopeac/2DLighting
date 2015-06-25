bcbbe5de497946d064e00541a988bb90f650f119
Andreas <andla827@student.liu.se>
2015-06-25 19:22:06 +0200

    Polygon split is working but the splitting planes seem to be offset.

--- Assets/Scripts/Occluder.cs
+++ Assets/Scripts/Occluder.cs
@@ -7,12 +7,15 @@ public class Occluder : MonoBehaviour
 {
     private List<GameObject> children;
     private new PolygonCollider2D collider;
+	private Vector2 position; 
+
+	private int c = 0;
 
     // Use this for initialization
     void Start()
     {
-
         children = new List<GameObject>();
+		position = transform.position;
 
         collider = GetComponent<PolygonCollider2D>();
         SplitConcaveColliders(collider, 0);
@@ -20,36 +23,55 @@ public class Occluder : MonoBehaviour
 
     protected void SplitConcaveColliders(PolygonCollider2D collider, int pathIndex)
     {
+		c++;
+
+		if (c > 20)
+			return;
+
         Vector2[] path = collider.GetPath(pathIndex);
         int[] conflicts = GetConflictingVertices(ref path);
 
-        Debug.Log(conflicts.Length);
-
+		//Do not proceed if there's no concave shape
         if (conflicts.Length == 0)
             return;
 
-        float x = path[(conflicts[0] + 1) % path.Length].x - path[conflicts[0]].x;
-        float y = path[(conflicts[0] + 1) % path.Length].y - path[conflicts[0]].y;
+		int current = conflicts [0];
+		int next = (current + 1) % path.Length;
 
-        Vector2 normal = new Vector3(y, -x);
+        float dx = path[next].x - path[current].x;
+        float dy = path[next].y - path[current].y;
+        Vector2 normal = new Vector3(-dy, dx);
 
-        Plane plane = new Plane(normal, path[conflicts[0]]);
-        Vector2[][] splits = PolygonUtils.Split(plane, path);
+        Plane plane = new Plane(normal, path[next]);
+        Vector2[][] splits = PolygonUtils.Split(plane, path, position);
 
-        PolygonCollider2D c1 = CreateSubCollider(splits[0]);
+		//Do not proceed if the split didn't result in two valid colliders
+		if (splits [0].Length < 3 || splits [1].Length < 3)
+			return;
+
+		Debug.Log ("SLOW" + splits[0].Length + " " + splits[1].Length);
+	
+		PolygonCollider2D c1 = CreateSubCollider(splits[0]);
         PolygonCollider2D c2 = CreateSubCollider(splits[1]);
 
+		//Keep going when it works
         SplitConcaveColliders(c1, 0);
         SplitConcaveColliders(c2, 0);
+
+
+
     }
 
-    protected PolygonCollider2D CreateSubCollider(Vector2[] path)
+    protected PolygonCollider2D CreateSubCollider(Vector2[] path, string name = "Collider")
     {
         GameObject child = new GameObject();
         child.transform.parent = this.transform;
+		child.transform.position = this.transform.position;
+		child.name = name;
+		children.Add(child);
+
         PolygonCollider2D collider = child.AddComponent<PolygonCollider2D>();
         collider.SetPath(0, path);
-        children.Add(child);
 
         return collider;
     }
--- Assets/Scripts/PolygonUtils.cs
+++ Assets/Scripts/PolygonUtils.cs
@@ -1,55 +1,67 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public class PolygonUtils
 {
 
-    public const int SPLITS = 2;
+	public const int SPLITS = 2;
 
-    public static Vector2[][] Split(Plane plane, Vector2[] path)
-    {
-        List<Vector2> negative = new List<Vector2>();
-        List<Vector2> positive = new List<Vector2>();
+	public static Vector2[][] Split (Plane split, Vector2[] path, Vector2 debug)
+	{
+		List<Vector2> negative = new List<Vector2> ();
+		List<Vector2> positive = new List<Vector2> ();
+		int length = path.Length;
 
-        int length = path.Length;
-        Vector2 previous = path[length - 1];
-        for (int i = 0; i < length; ++i)
-        {
-            Vector2 current = path[i];
+		//Set previous point in path
+		Vector2 previous = path [length - 1];
+		for (int i = 0; i < length; ++i) {
 
-            if (!plane.SameSide(current, previous))
-            {
-                Vector2 direction = current - previous;
-                Ray ray = new Ray(current, direction);
+			//Set current point in path
+			Vector2 current = path [i];
 
-                float distance;
-                plane.Raycast(ray, out distance);
+			bool sideCurrent = split.GetSide(current);
+			bool sidePrevious = split.GetSide(previous);
+	
+			if (sideCurrent != sidePrevious) {
 
-                Vector2 point = ray.GetPoint(distance);
-                if (Mathf.Abs(distance) > 0.0f && Mathf.Abs(distance) < 1.0f)
-                {
-                    negative.Add(point);
-                    positive.Add(point);
-                }
-            }
+				//Create a ray from the two points on different sides of the plane
+				Vector2 direction = current - previous;
+				Ray ray = new Ray (current, direction);
 
-            if (plane.GetSide(current))
-            {
-                positive.Add(current);
-            }
-            else
-            {
-                negative.Add(current);
-            }
+				//See where the ray intersects the plane
+				float distance;
+				split.Raycast (ray, out distance);
 
-            previous = current;
-        }
+				//Collect the point and add it to both shapes
+				Vector2 point = ray.GetPoint (distance);
+				
+				negative.Add (point);
+				positive.Add (point); 
 
-        Vector2[][] splits = new Vector2[SPLITS][];
-        splits[0] = positive.ToArray();
-        splits[1] = negative.ToArray();
+			}
 
-        return splits;
-    }
+			if (sideCurrent) {
+				positive.Add (current);
+			} else {
+				negative.Add (current);
+			}
+
+			previous = current;
+		}
+
+		Vector2[][] splits = new Vector2[SPLITS][];
+		splits [0] = positive.ToArray ();
+		splits [1] = negative.ToArray ();
+
+		return splits;
+	}
+
+	public static Vector2 GetNormal (Vector2 start, Vector2 end)
+	{
+		float dx = end.x - start.x;
+		float dy = end.y - start.y;
+
+		return new Vector2 (-dy, dx);
+	}
 }
