357dc7a076fb2b9171ab81bf421800a60dd8a7c8
Andreas Larsson <avopeac@gmail.com>
2015-07-23 22:11:23 +0200

    Added movement class for testing.

---
+++ Assets/Movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+    Vector2 position;
+
+    // Use this for initialization
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float x = 0.1f * Mathf.Cos(Mathf.PI * Time.fixedTime);
+        float y = 0.1f * Mathf.Sin(Mathf.PI * Time.fixedTime);
+
+        Vector3 pos = transform.position;
+        pos.x += x;
+        pos.y += y;
+
+        transform.position = pos;
+    }
+}
